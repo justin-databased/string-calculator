447fb5b25789dfb0945fa90b08c5aff7d6b25783
Justin Behnke <justin@databased.com>
2020-12-21 12:45:07 -0700

    All tests passing

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -5,12 +5,18 @@ class StringCalculator {
     }
 
     try {
-      return this.getNumberStringToSum(numberString)
-      .reduce((sum: number, numToAdd: string) => {
-        return sum + this.getValidNumber(numToAdd);
-      }, 0);
-    } catch(error) {
-      this.getNumberStringToSum(numberString).filter(num => parseInt(num, 10) < 0 ?)
+      return this.getNumberStringToSum(numberString).reduce(
+        (sum: number, numToAdd: string) => {
+          return sum + this.getValidNumber(numToAdd);
+        },
+        0
+      );
+    } catch (error) {
+      throw new Error(
+        `negatives not allowed: ${this.getNumberStringToSum(numberString)
+          .filter((num) => parseInt(num, 10) < 0)
+          .join(", ")}`
+      );
     }
   }
 
@@ -19,22 +25,22 @@ class StringCalculator {
     let numberStringToSum = numberString;
     if (numberString.includes("//")) {
       delimiter = numberString[2];
-      numberStringToSum = numberString.split('\n')[1]
+      numberStringToSum = numberString.split("\n")[1];
     }
 
-    return numberStringToSum.replace(/\n|\r/g, delimiter).split(delimiter)
+    return numberStringToSum.replace(/\n|\r/g, delimiter).split(delimiter);
   }
 
   private getValidNumber(numToAdd: string): number {
     if (Number.isNaN(parseInt(numToAdd, 10))) {
-      return 0
+      return 0;
     }
 
-    if(parseInt(numToAdd, 10) < 0) {
+    if (parseInt(numToAdd, 10) < 0) {
       throw new Error("negatives not allowed: ");
-    } 
+    }
 
-    return parseInt(numToAdd, 10)
+    return parseInt(numToAdd, 10);
   }
 }
 
